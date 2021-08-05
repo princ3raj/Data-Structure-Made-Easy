e11a5a80e00dd440ed4db0e60eb499631325feaa
princ3raj <princ3raj1999@gmail.com>
2021-08-05 12:50:12 +0530

    Done

---
+++ Queue/ImplementQueueUsingStack/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ImplementQueueUsingStack
+//
+//  Created by princeraj on 05/08/21.
+//
+
+/*
+ simply, take two stacks and start inserting element in s1, if you are about to delete then, first check s2 is empty, if its not then start popping it out, if its emptied then transfer all elements from s1 to s2 and carry out it further.
+ */
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
